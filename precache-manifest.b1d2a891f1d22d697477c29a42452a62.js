fd9ca6d3e5224ceecda00f0ce013f51ccc7d5a93
Edward Boland <mr.eboland@gmail.com>
2020-05-04 01:20:56 -0400

    Updates

---
+++ precache-manifest.b1d2a891f1d22d697477c29a42452a62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3678b8db24fad932adb57acc9c18702",
+    "url": "/project5/index.html"
+  },
+  {
+    "revision": "6f6a69e3edfe3ffd550f",
+    "url": "/project5/static/css/main.81e28179.chunk.css"
+  },
+  {
+    "revision": "8a31fe46b857ee6be811",
+    "url": "/project5/static/js/2.4067df1b.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/project5/static/js/2.4067df1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6a69e3edfe3ffd550f",
+    "url": "/project5/static/js/main.ffdb5d5f.chunk.js"
+  },
+  {
+    "revision": "b622877644f295aae65a",
+    "url": "/project5/static/js/runtime-main.513a01db.js"
+  },
+  {
+    "revision": "530b344a1a4ae4bf5ec9ff16cb6a61c2",
+    "url": "/project5/static/media/index1.530b344a.jpg"
+  },
+  {
+    "revision": "217e2a4d47bc603a22ad257e30626797",
+    "url": "/project5/static/media/index2.217e2a4d.jpg"
+  }
+]);
